ff0c7697876ee7306f9ae3a7114718e343cdea17
Asma <asma@dataseedtech.com>
2025-07-18 15:25:52 +0530

    refactor: update special character requirement message in password validation

--- src/components/table/common-tables/n-user/user-creation-form/user-form.schema.ts
+++ src/components/table/common-tables/n-user/user-creation-form/user-form.schema.ts
@@ -21,7 +21,7 @@ export const userSchema = z
       .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
       .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
       .regex(/[0-9]/, 'Password must contain at least one digit')
-      .regex(/[@$!%*?&]/, 'Password must contain at least one special character (@, $, !, %, *, ?, &)')
+      .regex(/[@$!%*?&]/, 'A special character is required (e.g., ! @ # $ % ^ & ).')
       .regex(/^(?!-)/, 'Password cannot start with a hyphen'),
     confirmPassword: z.string().min(6, 'Confirm password must be at least 6 characters'),
   })
