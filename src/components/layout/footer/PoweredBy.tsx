8826e6961333625cb3e6767d9651bd72b36acf26
Asma <asma@dataseedtech.com>
2025-08-22 16:33:24 +0530

    Powered by Dataseed' logo clickable

--- src/components/layout/footer/PoweredBy.tsx
+++ src/components/layout/footer/PoweredBy.tsx
@@ -4,7 +4,9 @@ import dataseedLogo from '@/assets/images/poweredby-dataseed-logo.svg';
 const PoweredBy = () => {
   return (
     <div className="flex justify-center items-center mt-16 mb-5">
-      <Image src={dataseedLogo} width={160} height={80} />
+      <a href="https://dataseedtech.com/" target="_blank" rel="noopener noreferrer">
+        <Image src={dataseedLogo} width={160} height={80} alt="Powered by Dataseed" />
+      </a>
     </div>
   );
 };
